25041827917be384e2daf6a49f1bd13039e7749d
Youssef Lasheen <youssef@lasheen.dev>
2022-12-10 14:01:47 +0200

    Set scaffold background to match gtk's in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,19 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: const Color(0xFFfafafa)
       ),
-      darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF242424)
+      ),
       home: const MyHomePage(title: 'GTK window Demo'),
     );
   }
