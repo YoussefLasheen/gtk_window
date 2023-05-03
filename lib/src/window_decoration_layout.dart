f959fe9667faeb7e7667a9c57102ee40f30f2345
Miroslav Mazel <contact.mirek@protonmail.com>
2023-05-03 10:33:28 +0200

    fromGTKTheme -> fromPlatform, for other platforms

--- lib/src/window_decoration_layout.dart
+++ lib/src/window_decoration_layout.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:gtk/gtk.dart';
 import 'package:gtk_window/src/window_decoration.dart';
 
@@ -7,17 +9,42 @@ class WindowDecorationLayout {
 
   const WindowDecorationLayout(this.leftItems, this.rightItems);
 
-  static WindowDecorationLayout fromGTKTheme() {
-    final layoutString =
-        GtkSettings().getProperty("gtk-decoration-layout") as String;
-    final splitSideStrings = layoutString.split(":");
-    final leftItemStrings = splitSideStrings.first.split(",");
-    final rightItemStrings = (splitSideStrings.length > 1)
-        ? splitSideStrings.last.split(",")
-        : <String>[];
+  static WindowDecorationLayout fromPlatform() {
+    if (Platform.isLinux) {
+      final layoutString =
+          GtkSettings().getProperty("gtk-decoration-layout") as String?;
+      if (layoutString != null) {
+        final splitSideStrings = layoutString.split(":");
+        final leftItemStrings = splitSideStrings.first.split(",");
+        final rightItemStrings = (splitSideStrings.length > 1)
+            ? splitSideStrings.last.split(",")
+            : <String>[];
 
-    return WindowDecorationLayout(_getDecorationsFromStrings(leftItemStrings),
-        _getDecorationsFromStrings(rightItemStrings));
+        return WindowDecorationLayout(
+            _getDecorationsFromStrings(leftItemStrings),
+            _getDecorationsFromStrings(rightItemStrings));
+      } else {
+        return const WindowDecorationLayout([], [
+          WindowDecoration.minimize,
+          WindowDecoration.maximize,
+          WindowDecoration.close
+        ]);
+      }
+    } else if (Platform.isMacOS) {
+      return const WindowDecorationLayout([
+        WindowDecoration.close,
+        WindowDecoration.minimize,
+        WindowDecoration.maximize
+      ], []);
+    } else if (Platform.isWindows) {
+      return const WindowDecorationLayout([], [
+        WindowDecoration.minimize,
+        WindowDecoration.maximize,
+        WindowDecoration.close
+      ]);
+    } else {
+      return const WindowDecorationLayout([], []);
+    }
   }
 
   static List<WindowDecoration> _getDecorationsFromStrings(
